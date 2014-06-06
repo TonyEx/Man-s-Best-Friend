a2696d02d5ca3d45232004e7f14579983fe07394
Tony Angelo <tonyangelox@veronica.home>
2014-06-06 19:26:15 -0400

    First commit

---
+++ Man's Best Friend/TEADog.h
@@ -0,0 +1,13 @@
+//
+//  TEADog.h
+//  Man's Best Friend
+//
+//  Created by Tony Angelo on 6/5/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TEADog : NSObject
+
+@end
