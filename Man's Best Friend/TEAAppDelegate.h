14ab66676bc63233feba68df029f9e6abae8b4f9
Tony Angelo <tonyangelox@veronica.home>
2014-06-05 21:23:44 -0400

    Initial Commit

---
+++ Man's Best Friend/TEAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TEAAppDelegate.h
+//  Man's Best Friend
+//
+//  Created by Tony Angelo on 6/5/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Man's Best Friend/TEAViewController.h
@@ -0,0 +1,13 @@
+//
+//  TEAViewController.h
+//  Man's Best Friend
+//
+//  Created by Tony Angelo on 6/5/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEAViewController : UIViewController
+
+@end
